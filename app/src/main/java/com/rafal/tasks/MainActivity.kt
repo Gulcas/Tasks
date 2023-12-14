2b99dddd2d24cc08a249d4d7976ae87c759f1625
Rafal Granoszewski <rafal.granoszewski@zalando.pl>
2023-12-14 18:49:01 +0100

    3

--- app/src/main/java/com/rafal/tasks/MainActivity.kt
+++ app/src/main/java/com/rafal/tasks/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.rafal.tasks
 
 import android.os.Bundle
+import android.widget.CheckBox
+import android.widget.RadioButton
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -25,19 +29,39 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Done
 import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.outlined.Email
+import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Divider
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.RadioButtonDefaults
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -69,7 +93,391 @@ class MainActivity : ComponentActivity() {
 //            MyProgress()
 //            MyViewExercise()
 //            MyButton()
-            MySurface()
+//            MySurface(
+//            MyCard()
+//            MyBox()
+//            MyLayoutWeight()
+//            MyLayoutExercise()
+//            MyClickButton()
+//            MyButtonClick()
+//            MyCheckBox()
+//            MyRadioButton()
+//            MySwitchButton()
+//            MySlider()
+//            MyIconButton()
+//            MyFAB()
+//            MyModifierClickable()
+//            MyShowHide()
+            MyShowHideExercise()
+        }
+    }
+    @Composable
+    fun MyShowHideExercise() {
+    }
+
+    @Composable
+    fun MyShowHide() {
+        var isShowed by remember { mutableStateOf(false) }
+
+        Row() {
+            Button(onClick = {isShowed = !isShowed}) {
+                Text(text = if(isShowed)"Pokazany" else "Ukryty")
+            }
+            if(isShowed) {
+                Text(text = "Ukryty tekst")
+            }
+        }
+    }
+
+    @Composable
+    fun MyModifierClickable() {
+        var isClicked by remember { mutableStateOf(false) }
+        Text(
+            text = if (isClicked) "Kliknięto" else "Nie kliknięto",
+            modifier = Modifier
+                .clickable { isClicked = !isClicked }
+        )
+    }
+
+    @Composable
+    fun MyFAB() {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            FloatingActionButton(
+                onClick = { },
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .padding(16.dp)
+            ) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Add Icon")
+            }
+        }
+    }
+
+    @Composable
+    fun MyIconButton() {
+        var isFavorite by remember { mutableStateOf(false) }
+        IconButton(onClick = { isFavorite = !isFavorite }) {
+            Icon(
+                imageVector = if (isFavorite) Icons.Filled.Favorite else Icons.Outlined.FavoriteBorder,
+                contentDescription = "Favorite Icon",
+                tint = Color.LightGray
+            )
+        }
+    }
+
+    @Composable
+    fun MySlider() {
+        var sliderValue by remember { mutableStateOf(0f) }
+        Slider(
+            value = sliderValue, onValueChange = { value -> sliderValue = value },
+            modifier = Modifier.padding(horizontal = 40.dp),
+            valueRange = 0f..100f,
+            steps = 15, //ilość kroków, kropek na suwaku
+            colors = SliderDefaults.colors(
+                activeTickColor = Color.Green,
+                inactiveTickColor = Color.Red,
+                thumbColor = Color.Black,
+                activeTrackColor = Color.Yellow,
+                inactiveTrackColor = Color.LightGray
+            )
+        )
+        Text(text = "$sliderValue")
+    }
+
+    @Composable
+    fun MySwitchButton() {
+        var isChecked by remember { mutableStateOf(false) }
+        Switch(
+            checked = isChecked, onCheckedChange = { value -> isChecked = value },
+            colors = SwitchDefaults.colors(
+                checkedBorderColor = Color.Green,
+                checkedThumbColor = Color.LightGray,
+                checkedTrackColor = Color.Yellow,
+                uncheckedBorderColor = Color.Black,
+                uncheckedThumbColor = Color.DarkGray,
+                uncheckedTrackColor = Color.Magenta
+            )
+        )
+    }
+
+    @Composable
+    fun MyRadioButton() {
+        var isSelected by remember { mutableStateOf(false) }
+        RadioButton(
+            selected = isSelected,
+            onClick = { isSelected = !isSelected }, //lambda
+            colors = RadioButtonDefaults.colors(
+                selectedColor = Color.Green,
+                unselectedColor = Color.Red
+            )
+        )
+    }
+
+    @Composable
+    fun MyCheckBox() {
+        var isChecked: Boolean by remember { mutableStateOf(false) }
+        Checkbox(
+            checked = isChecked,
+            onCheckedChange = { value -> isChecked = value }, //lambda
+            colors = CheckboxDefaults.colors(checkedColor = Color.Green, uncheckedColor = Color.Red)
+        )
+    }
+
+    @Composable
+    fun MyButtonClick() {
+        var number: Int by remember { mutableStateOf(0) }
+        Row() {
+            Button(
+                onClick = { number-- },
+                modifier = Modifier
+                    .size(30.dp),
+                contentPadding = PaddingValues(2.dp),
+                enabled = number > 0 //uproszczony if - jeśli warunek jest spełniony to przycisk jest aktywny
+            ) {
+                Text(text = "-")
+            }
+            Text(
+                text = "$number",
+                modifier = Modifier
+                    .size(height = 30.dp, width = 50.dp)
+                    .padding(horizontal = 4.dp)
+                    .border(1.dp, Color.Gray)
+                    .wrapContentSize()
+            )
+            Button(
+                onClick = { number++ },
+                modifier = Modifier
+                    .size(30.dp),
+                contentPadding = PaddingValues(2.dp)
+            ) {
+                Text(text = "+")
+            }
+        }
+    }
+
+    @Composable
+    fun MyClickButton() {
+        var countState: MutableState<Int> =
+            remember { mutableStateOf(0) } //zapisanie stanu zmiennej
+        var count: Int by remember { mutableStateOf(0) } //zapisanie stanu zmiennej poprzez delegat 'by'
+
+        Column {
+            Button(onClick = { countState.value++ }) {
+                Text(text = "Klikadełko ${countState.value}")
+            }
+            Button(onClick = { count++ }) {
+                Text(text = "Klikadełko versja druga $count")
+            }
+            MyInnerClickButton(countPar = count)
+            MyInnerClickButtonMuttable(countPar = countState)
+        }
+    }
+
+    @Composable
+    fun MyInnerClickButton(countPar: Int) {
+        Button(onClick = { /*countPar++ */ }) {
+            Text(text = "Inner count $countPar")
+        }
+    }
+
+    @Composable
+    fun MyInnerClickButtonMuttable(countPar: MutableState<Int>) {
+        Button(onClick = { countPar.value++ }) {
+            Text(text = "Inner count state ${countPar.value}")
+        }
+    }
+
+
+    @Composable
+    fun MyLayoutExercise() {
+        Box(
+            contentAlignment = Alignment.BottomCenter,
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            Row(
+                modifier = Modifier
+                    .background(Color.DarkGray)
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                MyLayoutInfoView(Modifier.weight(1f)) //przekazanie parametru do funkcji
+                MyLayoutPriceView()
+            }
+        }
+    }
+
+    @Composable
+    fun MyLayoutInfoView(modifier: Modifier) {
+        Column(modifier = modifier) {
+            Text(
+                text = "Kurs Kotlin i jakiś tam tekst długiiiiiiiiii",
+                color = Color.White,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "4.4",
+                    color = Color.Yellow,
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+                Icon(
+                    imageVector = Icons.Default.Star,
+                    contentDescription = "Star Icon",
+                    tint = Color.Yellow
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(
+                    text = "Ocen duźo",
+                    color = Color.Magenta,
+                    textDecoration = TextDecoration.Underline
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(
+                    text = "110 uczestników",
+                    color = Color.White
+                )
+            }
+        }
+    }
+
+    @Composable
+    fun MyLayoutPriceView() {
+        Row() {
+            Column(
+                horizontalAlignment = Alignment.End,
+            ) {
+                Text(
+                    text = "44,99 zł",
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White,
+                    fontSize = 20.sp
+                )
+                Text(
+                    text = "244,99 zł",
+                    color = Color.LightGray,
+                    textDecoration = TextDecoration.LineThrough,
+                    fontSize = 14.sp
+                )
+            }
+            Spacer(modifier = Modifier.width(16.dp))
+            Button(
+                onClick = {},
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.White,
+                    contentColor = Color.Black
+                ),
+                shape = RoundedCornerShape(5.dp),
+                contentPadding = PaddingValues(8.dp),
+            ) {
+                Text(
+                    text = "Kup teraz",
+                    fontWeight = FontWeight.ExtraBold
+                )
+            }
+        }
+    }
+
+    @Composable
+    fun MyLayoutWeight() {
+        Row() {
+            Text(
+                text = "text 1",
+                modifier = Modifier
+                    .background(Color.Green)
+                    .weight(1f)
+            )
+            Text(
+                text = "text 2",
+                modifier = Modifier
+                    .background(Color.Yellow)
+//                    .weight(1f)
+            )
+            Text(
+                text = "text 3",
+                modifier = Modifier
+                    .background(Color.Red)
+//                    .weight(1f)
+            )
+
+        }
+    }
+
+    @Composable
+    fun MyBox() {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.LightGray),
+//            contentAlignment = Alignment.TopEnd
+        ) {
+            Text(
+                text = "Box",
+                modifier = Modifier
+                    .align(Alignment.TopCenter)
+            )
+            Text(
+                text = "Box2",
+                modifier = Modifier
+                    .align(Alignment.TopStart)
+            )
+            Text(
+                text = "Box3",
+                modifier = Modifier
+                    .align(Alignment.TopEnd)
+            )
+            Box(
+                modifier = Modifier
+                    .size(150.dp)
+                    .border(1.dp, Color.Black, RoundedCornerShape(8.dp))
+                    .align(Alignment.Center)
+                    .padding(10.dp)
+            ) {
+
+                Spacer(
+                    modifier = Modifier
+                        .size(50.dp)
+                        .background(Color.Green, RoundedCornerShape(8.dp))
+                        .align(Alignment.TopStart)
+                )
+                Spacer(
+                    modifier = Modifier
+                        .size(70.dp)
+                        .background(Color.Yellow, RoundedCornerShape(8.dp))
+                        .align(Alignment.BottomEnd)
+                )
+                Spacer(
+                    modifier = Modifier
+                        .size(100.dp)
+                        .background(Color.Red, RoundedCornerShape(8.dp))
+                        .align(Alignment.Center)
+                )
+            }
+        }
+    }
+
+    @Composable
+    fun MyCard() {
+        Card(
+            modifier = Modifier
+                .padding(20.dp),
+            elevation = CardDefaults.elevatedCardElevation(defaultElevation = 10.dp),
+            colors = CardDefaults.cardColors(Color.DarkGray)//cieniowanie
+        ) {
+            Text(
+                text = "Card",
+                modifier = Modifier
+                    .padding(10.dp)
+            )
         }
     }
 
